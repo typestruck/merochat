c9ed70396a3098a3ff3e443ee586d3e0f03e9a2e
Eduardo Asafe <e@asafe.dev>
2019-09-29 02:21:27 +0200

    Suggestions tests

---
+++ test/Client/IM/Suggestions.purs
@@ -0,0 +1,12 @@
+module Test.Client.Suggestions where
+
+import Prelude
+import Shared.Types
+
+import Data.Maybe (Maybe(..))
+import Test.Unit (TestSuite)
+import Test.Unit as TU
+import Test.Unit.Assert as TUA
+import Test.Unit.Main as TUM
+
+sdafdfasd
