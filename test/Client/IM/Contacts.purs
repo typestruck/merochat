2921c1b1f113d226ee90d54f0ddd9f19dc3118c3
Eduardo Asafe <e@asafe.dev>
2020-07-11 17:16:25 +0800

    Updated tests

--- test/Client/IM/Contacts.purs
+++ test/Client/IM/Contacts.purs
@@ -1,85 +1,59 @@
 module Test.Client.IM.Contacts where
 
-import Client.Common.Types
-import Prelude
-import Shared.IM.Types
-import Shared.Types
-
 import Client.IM.Contacts as CICN
-import Data.Int53 as DI
 import Data.Maybe (Maybe(..))
 import Data.Newtype as DN
 import Data.Tuple (Tuple(..))
+import Data.Tuple as DT
 import Effect.Now as EN
 import Effect.Unsafe as EU
+import Prelude (discard, map, ($), (<<<))
+import Shared.IM.Types (Contact, HistoryMessage(..), IMModel(..), MessageStatus(..))
 import Shared.Newtype as SN
 import Shared.PrimaryKey as SP
+import Shared.Types (MDateTime(..), PrimaryKey(..))
 import Shared.Unsafe ((!@))
+import Test.Client.Model (contact, imUser)
 import Test.Client.Model as TCM
 import Test.Unit (TestSuite)
 import Test.Unit as TU
 import Test.Unit.Assert as TUA
-import Test.Unit.Main as TUM
-import Unsafe.Coerce (unsafeCoerce)
-import Web.Socket.WebSocket (WebSocket)
 
 tests :: TestSuite
 tests = do
         TU.suite "im contacts update" $ do
                 TU.test "resumeChat resets suggesting" $ do
-                        IMModel { suggesting } <- CICN.resumeChat (SP.fromInt 23) <<< SN.updateModel model $ _ {
+                        let IMModel { suggesting } = DT.fst <<< CICN.resumeChat anotherContactID <<< SN.updateModel model $ _ {
                                 suggesting = Just 4455
                         }
                         TUA.equal Nothing suggesting
 
                 TU.test "resumeChat sets chatting" $ do
-                        m@(IMModel { chatting }) <- CICN.resumeChat (SP.fromInt 32) <<< SN.updateModel model $ _ {
+                        let m@(IMModel { chatting }) = DT.fst <<< CICN.resumeChat (SP.fromInt 32) <<< SN.updateModel model $ _ {
                                 chatting = Nothing
                         }
                         TUA.equal (Just 1) chatting
 
-                        IMModel { chatting } <- CICN.resumeChat (SP.fromInt 23) m
+                        let IMModel { chatting } = DT.fst $ CICN.resumeChat anotherContactID m
                         TUA.equal (Just 0) chatting
+
                 TU.test "markRead sets recieved messages as read" $ do
-                        IMModel { contacts } <- TCM.run model <<< CICN.markRead webSocketHandler $ SN.updateModel model $ _ {
+                        let IMModel { contacts } = DT.fst <<< CICN.markRead <<< SN.updateModel model $ _ {
                                 chatting = Just 1
                         }
                         TUA.equal [Tuple (SP.fromInt 1) Read, Tuple (SP.fromInt 2) Unread, Tuple (SP.fromInt 3) Read] <<< map (\(HistoryMessage { id, status}) -> Tuple id status) <<< _.history $ DN.unwrap (contacts !@ 1)
 
 model :: IMModel
-model = IMModel {
-        userContextMenuVisible: false,
-        profileSettingsToggle: Hidden,
-        contacts: [imUser, anotherIMUser],
-        user: imUser,
-        suggestions: [],
-        contactsPage: 0,
-        temporaryID : SP.fromInt 0,
-        token: Just "oi",
-        webSocket: Just $ WS (unsafeCoerce 23 :: WebSocket),
-        suggesting: Just 0,
-        chatting: Nothing
+model = SN.updateModel TCM.model $ _ {
+        contacts = [contact, anotherContact]
 }
 
-imUser :: IMUser
-imUser = IMUser {
-        age: Nothing,
-        name: "test",
-        id: SP.fromInt 23,
-        avatar: "",
-        country: Nothing,
-        languages: [],
-        tags: [],
-        message: "",
-        history: [],
-        headline: "",
-        description: "",
-        gender: Nothing
-}
+anotherContactID :: PrimaryKey
+anotherContactID = SP.fromInt 23
 
-anotherIMUser :: IMUser
-anotherIMUser = SN.updateUser imUser $ _ {
-        id = SP.fromInt 32,
+anotherContact :: Contact
+anotherContact = SN.updateContact contact $ _ {
+        user = SN.updateUser imUser $ _ { id = anotherContactID },
         history = [
                 HistoryMessage {
                        id: SP.fromInt 1,
@@ -107,6 +81,3 @@ anotherIMUser = SN.updateUser imUser $ _ {
                 }
         ]
 }
-
-webSocketHandler :: WebSocketHandler
-webSocketHandler = { sendPayload: \_ _ -> pure unit }
--- test/Client/Model.purs
+++ test/Client/Model.purs
@@ -1,9 +1,70 @@
 module Test.Client.Model where
 
+import Data.Maybe (Maybe(..))
 import Effect.Aff (Aff)
-import Prelude
+import Prelude (bind, pure, ($))
+import Shared.IM.Types (Contact(..), IMModel(..), IMUser(..), ProfileSettingsToggle(..), Suggestion, WS(..))
+import Shared.Newtype as SN
+import Shared.PrimaryKey as SP
+import Shared.Types (PrimaryKey(..))
+import Unsafe.Coerce as UC
+import Web.Socket.WebSocket (WebSocket)
 
 run :: forall m. m -> Aff (m -> m) -> Aff m
 run model f = do
         f' <- f
         pure $ f' model
+
+
+model :: IMModel
+model = IMModel {
+        userContextMenuVisible: false,
+        profileSettingsToggle: Hidden,
+        user: imUser,
+        suggestions: [suggestion],
+        temporaryID : SP.fromInt 0,
+        suggesting: Just 0,
+        contactsPage: 0,
+        token: Just "",
+        contacts: [contact],
+        webSocket: Just $ WS (UC.unsafeCoerce 23 :: WebSocket),
+        chatting: Just 0
+}
+
+imUserID :: PrimaryKey
+imUserID = SP.fromInt 23
+
+imUser :: IMUser
+imUser = IMUser {
+        age: Nothing,
+        name: "test",
+        id: imUserID,
+        avatar: Nothing,
+        country: Nothing,
+        languages: [],
+        tags: [],
+        headline: "",
+        description: "",
+        gender: Nothing,
+        karma: 5
+}
+
+anotherIMUserID :: PrimaryKey
+anotherIMUserID = SP.fromInt 90
+
+anotherIMUser :: IMUser
+anotherIMUser = SN.updateUser imUser $ _ { id = anotherIMUserID }
+
+contact :: Contact
+contact = Contact {
+        chatStarter: imUserID,
+        user: anotherIMUser,
+        history: [],
+        chatAge: 0.0
+}
+
+suggestionID :: PrimaryKey
+suggestionID = SP.fromInt 300
+
+suggestion :: Suggestion
+suggestion = SN.updateUser imUser $ _ { id = suggestionID }
