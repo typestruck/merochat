460f871b7752d93576d267e59a68a7fd271356f4
Eduardo Asafe <e@asafe.dev>
2020-07-30 11:47:18 +0800

    Internal server error page (#115)

--- src/Server/Main.purs
+++ src/Server/Main.purs
@@ -1,17 +1,20 @@
 module Server.Main where
 
-import Prelude (Unit, bind, const, discard, show, void, ($), (<>))
-import Server.Types (Configuration(..))
-
+import Control.Monad.Error.Class as CMEC
 import Data.Map as DM
 import Effect (Effect)
 import Effect.Class (liftEffect)
 import Effect.Console as EC
+import Effect.Exception as EE
 import Effect.Ref as ER
 import HTTPure as H
+import Prelude
 import Server.Configuration as CF
 import Server.Database as SD
+import Server.InternalError.Template as SIT
+import Server.Response as SRR
 import Server.Router as SR
+import Server.Types (Configuration(..), ContentType(..))
 import Server.WebSocket (Port(..))
 import Server.WebSocket as SW
 import Server.WebSocket.Events as SWE
@@ -19,25 +22,29 @@ import Shared.WebSocketOptions (port)
 
 main :: Effect Unit
 main = do
-        configuration <- CF.readConfiguration
-        startWebSocketServer configuration
-        startHTTPServer configuration
+      configuration <- CF.readConfiguration
+      startWebSocketServer configuration
+      startHTTPServer configuration
 
 startWebSocketServer :: Configuration -> Effect Unit
 startWebSocketServer configuration = do
-        allConnections <- ER.new DM.empty
+      allConnections <- ER.new DM.empty
 
-        webSocketServer <- SW.createWebSocketServerWithPort (Port port) {} $ const (EC.log $ "WebSocket now up on ws://localhost:" <> show port)
-        SW.onServerError webSocketServer SWE.handleError
-        pool <- SD.newPool
-        SW.onConnection webSocketServer $ SWE.handleConnection { configuration, pool, allConnections }
+      webSocketServer <- SW.createWebSocketServerWithPort (Port port) {} $ const (EC.log $ "WebSocket now up on ws://localhost:" <> show port)
+      SW.onServerError webSocketServer SWE.handleError
+      pool <- SD.newPool
+      SW.onConnection webSocketServer $ SWE.handleConnection { configuration, pool, allConnections }
 
 startHTTPServer :: Configuration -> Effect Unit
 startHTTPServer c@(Configuration configuration) = do
-        pool <- SD.newPool
-        void $ H.serve configuration.port (router pool) $ EC.log ("Server now up on http://localhost:" <> show configuration.port)
+      pool <- SD.newPool
+      void $ H.serve configuration.port (router pool) $ EC.log ("Server now up on http://localhost:" <> show configuration.port)
 
-        where   router pool request@{ path } = do
-                        session <- liftEffect $ SR.session c request
-                        SR.runRouter { configuration: c, pool, session } request
+      where internalError error = do
+                  contents <- liftEffect <<< SIT.template $ EE.message error
+                  H.ok' (SRR.headerContentType $ show HTML) contents
+            router pool request@{ path } = do
+                  session <- liftEffect $ SR.session c request
+                  SR.runRouter { configuration: c, pool, session } request `CMEC.catchError` internalError
+
 
