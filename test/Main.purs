a986d39d09329733360d66572f6038cee1ce45a1
Eduardo Asafe <e@asafe.dev>
2021-01-01 13:27:59 +0800

    Remove integration tests from cli

--- test/Main.purs
+++ test/Main.purs
@@ -2,13 +2,17 @@ module Test.Main where
 
 import Prelude
 
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
+import Node.Process as NP
 import Test.Client.Main as TCM
 import Test.Server.Main as TSM
 import Test.Unit.Main as TUM
 
 main :: Effect Unit
-main = TUM.runTest do
-      TSM.tests
-      TCM.tests
+main = do
+      cli <- NP.lookupEnv "CLI"
+      TUM.runTest do
+            unless (cli == Just "true") TSM.tests
+            TCM.tests
 
