318af539c0932d26a76f7905b6ce653e0bbf854b
Eduardo Asafe <e@asafe.dev>
2021-01-27 16:33:10 +0800

    Dont show message status for received messages

--- production/Environment.purs
+++ production/Environment.purs
@@ -4,13 +4,13 @@ development :: Boolean
 development = false
 
 commonJSHash :: String
-commonJSHash = "a06764b805ec6c997fbe"
+commonJSHash = "599befac75bb13a6b48e"
 otherJSHash :: String
-otherJSHash = "8f875e7f40cf10076b8d"
+otherJSHash = "ea80e5846b6b62f1cf77"
 
 imJSHash :: String
-imJSHash = "5d3df84694443a823713"
+imJSHash = "3f19806d61f72401f431"
 imCSSHash :: String
-imCSSHash = "764a972cf23262537d6d"
+imCSSHash = "da02bcb04782805b59d2"
 emojiJSHash :: String
 emojiJSHash = "579d0f135d969603bd63"
