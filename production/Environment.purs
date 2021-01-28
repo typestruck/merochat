87356e776a99f39809624d609bb9168cc1f70066
Eduardo Asafe <e@asafe.dev>
2021-01-28 17:19:36 +0800

    Fix checking for missed messages (#168)

--- production/Environment.purs
+++ production/Environment.purs
@@ -5,12 +5,15 @@ development = false
 
 commonJSHash :: String
 commonJSHash = "599befac75bb13a6b48e"
+
 otherJSHash :: String
 otherJSHash = "ea80e5846b6b62f1cf77"
 
 imJSHash :: String
 imJSHash = "3f19806d61f72401f431"
+
 imCSSHash :: String
 imCSSHash = "da02bcb04782805b59d2"
+
 emojiJSHash :: String
 emojiJSHash = "579d0f135d969603bd63"
