17568f0a1fefdb9e5b625a6fb6e707eabba2ed1e
Eduardo Asafe <e@asafe.dev>
2021-02-04 15:40:49 +0800

    Fix leaderboad positions

--- src/Server/Leaderboard/Database.purs
+++ src/Server/Leaderboard/Database.purs
@@ -14,4 +14,4 @@ userPosition :: PrimaryKey -> ServerEffect Int
 userPosition loggedUserID = SD.scalar' (Query "select position from karma_leaderboard where ranker = $1") $ Row1 loggedUserID
 
 fetchInBetween10 :: Int -> ServerEffect (Array LeaderboardUserWrapper)
-fetchInBetween10 position = SD.select (Query "select u.name, u.avatar, position, current_karma karma from karma_leaderboard k join users u on k.ranker = u.id where position >= $1 - 5 and position <= $1 + 5") $ Row1 position
+fetchInBetween10 position = SD.select (Query "select u.name, u.avatar, position, current_karma karma from karma_leaderboard k join users u on k.ranker = u.id where position between greatest(1, $1 - 5) and $1 + 5 order by position") $ Row1 position
