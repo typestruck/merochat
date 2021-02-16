5c339e202c1c9c6845cc7b4bcaaa66ecb71496df
Eduardo Asafe <e@asafe.dev>
2021-02-16 13:32:14 +0800

    Fix backing page mobile style; do not show notification promt if denied; scroll into message when loading chat history

--- production/Environment.purs
+++ production/Environment.purs
@@ -7,13 +7,13 @@ commonJSHash :: String
 commonJSHash = "9b549c43131f409c7cc1"
 
 otherJSHash :: String
-otherJSHash = "71f305066fddd1f9e469"
+otherJSHash = "81545c4c9f1f111dc7e4"
 
 imCSSHash :: String
 imCSSHash = "ba50556a8bdc652771bb"
 
 imJSHash :: String
-imJSHash = "a77cdb4e895338c272ee"
+imJSHash = "649cabe74c0a60a084b6"
 
 emojiJSHash :: String
 emojiJSHash = "579d0f135d969603bd63"
@@ -22,7 +22,7 @@ helpJSHash :: String
 helpJSHash = "e2127064dcd520f2a977"
 
 baseJSHash :: String
-baseJSHash = "2a6d2314a87b3f0f1834"
+baseJSHash = "768271ce06fa68cc4b3d"
 
 backerCSSHash :: String
-backerCSSHash = "5c2d5d65952114e0b0e3"
+backerCSSHash = "0c01c6efe2f4a165e866"
