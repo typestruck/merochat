4fcd400ef154a92813229f4a173b69d9c0c8eb97
Eduardo Asafe <e@asafe.dev>
2021-02-23 16:33:43 +0800

    Updated hashs

--- production/Environment.purs
+++ production/Environment.purs
@@ -3,29 +3,66 @@ module Environment where
 development :: Boolean
 development = false
 
-commonJSHash :: String
-commonJSHash = "9b549c43131f409c7cc1"
-
-otherJSHash :: String
-otherJSHash = "2854b350e6ff1ee7e8f7"
-
-imCSSHash :: String
-imCSSHash = "e8c321be5968fa2a95dc"
-
-imJSHash :: String
-imJSHash = "dc9d8cdc5b0a52f9408f"
-
-emojiJSHash :: String
-emojiJSHash = "579d0f135d969603bd63"
-
-helpJSHash :: String
-helpJSHash = "e2127064dcd520f2a977"
-
-baseJSHash :: String
-baseJSHash = "768271ce06fa68cc4b3d"
 
 backerCSSHash :: String
 backerCSSHash = "0c01c6efe2f4a165e866"
 
+baseCSSHash :: String
+baseCSSHash = "768271ce06fa68cc4b3d"
+
+commonJSHash :: String
+commonJSHash = "9b549c43131f409c7cc1"
+
+emojiJSHash :: String
+emojiJSHash = "579d0f135d969603bd63"
+
+externalCSSHash :: String
+externalCSSHash = "9361845d640d2fa7ac80"
+
+helpCSSHash :: String
+helpCSSHash = "5c2d5d65952114e0b0e3"
+
+helpJSHash :: String
+helpJSHash = "e2127064dcd520f2a977"
+
+imJSHash :: String
+imJSHash = "dc9d8cdc5b0a52f9408f"
+
+imCSSHash :: String
+imCSSHash = "e8c321be5968fa2a95dc"
+
+internalHelpJSHash :: String
+internalHelpJSHash = "efc84b6e78a087e436fc"
+
+landingCSSHash :: String
+landingCSSHash = "b437cef6db9d74e21536"
+
+landingJSHash :: String
+landingJSHash = "e73af710139b0b4b7561"
+
+leaderboardJSHash :: String
+leaderboardJSHash = "12d1a05a1c90f39999a3"
+
 leaderboardCSSHash :: String
 leaderboardCSSHash = "f11d73f5cc7006586e2c"
+
+loginJSHash :: String
+loginJSHash = "987f0b7b7b052fcf9ad2"
+
+otherJSHash :: String
+otherJSHash = "2854b350e6ff1ee7e8f7"
+
+profileCSSHash :: String
+profileCSSHash = "ac60b6d1cf56812f13e6"
+
+profileJSHash :: String
+profileJSHash = "ecb96820b228ebb3c794"
+
+recoverJSHash :: String
+recoverJSHash = "1a5fa8368956a8294e65"
+
+settingsCSSHash :: String
+settingsCSSHash = "6390cea6e61528add6f3"
+
+settingsJSHash :: String
+settingsJSHash = "99057a3dd05954003021"
