6519a273178b93ad95a077cb8a2a9f35673c9dc7
Eduardo Asafe <e@asafe.dev>
2021-03-10 18:10:19 +0800

    Updated hashs

--- development/Environment.purs
+++ development/Environment.purs
@@ -4,6 +4,9 @@ development :: Boolean
 development = true
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> d01a15c... Updated hashs
 
 backerCSSHash :: String
 backerCSSHash = "0c01c6efe2f4a165e866"
@@ -12,11 +15,24 @@ baseCSSHash :: String
 baseCSSHash = "768271ce06fa68cc4b3d"
 
 commonJSHash :: String
+<<<<<<< HEAD
 commonJSHash = "d9f0622bac7d058268e1"
+=======
+commonJSHash = "750c3a54649f9b231092"
+>>>>>>> d01a15c... Updated hashs
 
 emojiJSHash :: String
 emojiJSHash = "579d0f135d969603bd63"
 
+<<<<<<< HEAD
+=======
+experimentsCSSHash :: String
+experimentsCSSHash = "47eff52b0d363fd4de3e"
+
+experimentsJSHash :: String
+experimentsJSHash = "daf689cefc842d85daf5"
+
+>>>>>>> d01a15c... Updated hashs
 externalCSSHash :: String
 externalCSSHash = "9361845d640d2fa7ac80"
 
@@ -26,11 +42,19 @@ helpCSSHash = "5c2d5d65952114e0b0e3"
 helpJSHash :: String
 helpJSHash = "e2127064dcd520f2a977"
 
+<<<<<<< HEAD
 imCSSHash :: String
 imCSSHash = "7f6619904ed8dd732a7a"
 
 imJSHash :: String
 imJSHash = "ded32fc46ce55374aca2"
+=======
+imJSHash :: String
+imJSHash = "dcc249695a52214b42b0"
+
+imCSSHash :: String
+imCSSHash = "ea57e1a66d5d4c0329cb"
+>>>>>>> d01a15c... Updated hashs
 
 internalHelpJSHash :: String
 internalHelpJSHash = "efc84b6e78a087e436fc"
@@ -51,6 +75,7 @@ loginJSHash :: String
 loginJSHash = "987f0b7b7b052fcf9ad2"
 
 otherJSHash :: String
+<<<<<<< HEAD
 otherJSHash = "2854b350e6ff1ee7e8f7"
 
 profileCSSHash :: String
@@ -58,6 +83,15 @@ profileCSSHash = "ac60b6d1cf56812f13e6"
 
 profileJSHash :: String
 profileJSHash = "ecb96820b228ebb3c794"
+=======
+otherJSHash = "4f94fb3e5dac372c8c67"
+
+profileJSHash :: String
+profileJSHash = "000e0eecae3a3503c340"
+
+profileCSSHash :: String
+profileCSSHash = "64a151ddf74b7372087a"
+>>>>>>> d01a15c... Updated hashs
 
 recoverJSHash :: String
 recoverJSHash = "1a5fa8368956a8294e65"
@@ -67,6 +101,7 @@ settingsCSSHash = "6390cea6e61528add6f3"
 
 settingsJSHash :: String
 settingsJSHash = "99057a3dd05954003021"
+<<<<<<< HEAD
 
 experimentsJSHash :: String
 experimentsJSHash = ""
@@ -75,3 +110,5 @@ experimentsCSSHash :: String
 experimentsCSSHash = ""
 =======
 >>>>>>> a6f2005... Updated hashs
+=======
+>>>>>>> d01a15c... Updated hashs
