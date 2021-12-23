d53f68e451f204450586bc10405501e8aa0e3611
Eduardo Asafe <e@asafe.dev>
2021-12-23 16:55:11 +0800

    Update hashs

--- production/Environment.purs
+++ production/Environment.purs
@@ -11,7 +11,7 @@ baseCSSHash :: String
 baseCSSHash = "768271ce06fa68cc4b3d"
 
 commonJSHash :: String
-commonJSHash = "11f83f9be4f14621008e"
+commonJSHash = "7e18b6a4f01d34f71db0"
 
 emojiJSHash :: String
 emojiJSHash = "16c6eb82ac41c946ac97"
@@ -20,7 +20,7 @@ experimentsCSSHash :: String
 experimentsCSSHash = "47eff52b0d363fd4de3e"
 
 experimentsJSHash :: String
-experimentsJSHash = "9f95468cf9ca906b4f32"
+experimentsJSHash = "add07bfa1ffbb47b0769"
 
 externalCSSHash :: String
 externalCSSHash = "9361845d640d2fa7ac80"
@@ -31,11 +31,11 @@ helpJSHash = "3f716dce97c4d5146de1"
 helpCSSHash :: String
 helpCSSHash = "5c2d5d65952114e0b0e3"
 
-imCSSHash :: String
-imCSSHash = "6a16a59a1c24f1d3d3c7"
-
 imJSHash :: String
-imJSHash = "d1afc9206fc56b7c5a09"
+imJSHash = "2e11b6602faf679e1a41"
+
+imCSSHash :: String
+imCSSHash = "df1d247b914f90a07679"
 
 internalHelpJSHash :: String
 internalHelpJSHash = "0bcde1006229fc18c71f"
@@ -55,17 +55,17 @@ leaderboardCSSHash = "f11d73f5cc7006586e2c"
 loginJSHash :: String
 loginJSHash = "75000e624ca8e24e0fa3"
 
-profileCSSHash :: String
-profileCSSHash = "64a151ddf74b7372087a"
-
 profileJSHash :: String
-profileJSHash = "b9fa9371e06a164e9312"
+profileJSHash = "3075d5e8f26ee1c5184d"
+
+profileCSSHash :: String
+profileCSSHash = "8bb2bac1ef7248e8a134"
 
 recoverJSHash :: String
 recoverJSHash = "e9897db66813a724ea75"
 
+settingsJSHash :: String
+settingsJSHash = "536ef2ce55dfa57cddd3"
+
 settingsCSSHash :: String
 settingsCSSHash = "6390cea6e61528add6f3"
-
-settingsJSHash :: String
-settingsJSHash = "b9d4968567e485a38e75"
