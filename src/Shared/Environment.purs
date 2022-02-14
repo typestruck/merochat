022ddc66cd908ff5130ebf4b1009418235930306
Eduardo Asafe <e@asafe.dev>
2022-02-14 20:02:08 -0300

    Update hashs

---
+++ src/Shared/Environment.purs
@@ -0,0 +1,70 @@
+module Environment where
+
+development :: Boolean
+development = false
+
+backerCSSHash :: String
+backerCSSHash = "0c01c6efe2f4a165e866"
+
+baseCSSHash :: String
+baseCSSHash = "768271ce06fa68cc4b3d"
+
+commonJSHash :: String
+commonJSHash = "944fb7b39983fc2511f8"
+
+emojiJSHash :: String
+emojiJSHash = "16c6eb82ac41c946ac97"
+
+experimentsCSSHash :: String
+experimentsCSSHash = "47eff52b0d363fd4de3e"
+
+experimentsJSHash :: String
+experimentsJSHash = "add07bfa1ffbb47b0769"
+
+externalCSSHash :: String
+externalCSSHash = "9361845d640d2fa7ac80"
+
+helpJSHash :: String
+helpJSHash = "3f716dce97c4d5146de1"
+
+helpCSSHash :: String
+helpCSSHash = "5c2d5d65952114e0b0e3"
+
+imJSHash :: String
+imJSHash = "2e11b6602faf679e1a41"
+
+imCSSHash :: String
+imCSSHash = "df1d247b914f90a07679"
+
+internalHelpJSHash :: String
+internalHelpJSHash = "0bcde1006229fc18c71f"
+
+landingJSHash :: String
+landingJSHash = "3fd7d8c6fd811c3ce01c"
+
+landingCSSHash :: String
+landingCSSHash = "b437cef6db9d74e21536"
+
+leaderboardJSHash :: String
+leaderboardJSHash = "a64a56c1ecbfaed09144"
+
+leaderboardCSSHash :: String
+leaderboardCSSHash = "f11d73f5cc7006586e2c"
+
+loginJSHash :: String
+loginJSHash = "75000e624ca8e24e0fa3"
+
+profileJSHash :: String
+profileJSHash = "3075d5e8f26ee1c5184d"
+
+profileCSSHash :: String
+profileCSSHash = "8bb2bac1ef7248e8a134"
+
+recoverJSHash :: String
+recoverJSHash = "e9897db66813a724ea75"
+
+settingsJSHash :: String
+settingsJSHash = "536ef2ce55dfa57cddd3"
+
+settingsCSSHash :: String
+settingsCSSHash = "6390cea6e61528add6f3"
