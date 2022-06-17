22bdb92fd4413c577d798e9d9295821fd56f9d43
Eduardo Asafe <e@asafe.dev>
2022-06-16 23:50:19 -0300

    Update hashs

--- src/Shared/Environment.purs
+++ src/Shared/Environment.purs
@@ -10,7 +10,7 @@ baseCSSHash :: String
 baseCSSHash = "be06c5112acf188d4cd8"
 
 commonJSHash :: String
-commonJSHash = "4a1b99d49087ee564660"
+commonJSHash = "d29b3b4c95c0917f4229"
 
 emojiJSHash :: String
 emojiJSHash = "2095717c9f496fac2e82"
@@ -19,7 +19,7 @@ experimentsCSSHash :: String
 experimentsCSSHash = "2e9a38a44858c519247a"
 
 experimentsJSHash :: String
-experimentsJSHash = "a2623747fe7a174d2a3b"
+experimentsJSHash = "32940d0ebf5e9275bd82"
 
 externalCSSHash :: String
 externalCSSHash = "6aa2e5074de8c9abb205"
@@ -28,37 +28,43 @@ helpCSSHash :: String
 helpCSSHash = "0c86b8fed90c6933a556"
 
 helpJSHash :: String
-helpJSHash = "62d8aa4d3b00ea1f0113"
+helpJSHash = "d15db41469a6ac3edaa6"
+
+imJSHash :: String
+imJSHash = "5e2e9db1e88d6e66898f"
 
 imCSSHash :: String
 imCSSHash = "f755b25488f66577d761"
 
-imJSHash :: String
-imJSHash = "f815d31b25ad86bd5277"
-
 internalHelpJSHash :: String
-internalHelpJSHash = "78ea3d5c712fe04a995c"
+internalHelpJSHash = "33d6d95156af7e7720a5"
+
+landingJSHash :: String
+landingJSHash = "4de6c4fe7bcfeb2009ee"
 
 leaderboardJSHash :: String
-leaderboardJSHash = "439acfb99ba4353073da"
+leaderboardJSHash = "6aa95d03c0368e95cdbe"
 
 leaderboardCSSHash :: String
 leaderboardCSSHash = "77e6e27fc9961a9dba4d"
 
 loginJSHash :: String
-loginJSHash = "d4f9447db213a229a602"
+loginJSHash = "288d797ec1bcc2ad53b3"
 
 profileCSSHash :: String
 profileCSSHash = "39014d6d4937b2223369"
 
 profileJSHash :: String
-profileJSHash = "f59f3018674d713e6edf"
+profileJSHash = "d4d28531c74c023e8298"
 
 recoverJSHash :: String
-recoverJSHash = "e88ba549017a49fe3eb5"
+recoverJSHash = "64f63135e01743e6bd01"
 
 settingsJSHash :: String
-settingsJSHash = "c8fd5219d34c1196d982"
+settingsJSHash = "3a0fc94437e76309663e"
 
 settingsCSSHash :: String
 settingsCSSHash = "e67c3e5f2f7fee54a27f"
+
+styleCSSHash :: String
+styleCSSHash = ""
