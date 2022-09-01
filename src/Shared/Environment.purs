a635399b1c5f87d3848480da39893bd357718981
Eduardo Asafe <e@asafe.dev>
2022-09-01 16:48:05 +0400

    Update hashs [skip ci]

--- src/Shared/Environment.purs
+++ src/Shared/Environment.purs
@@ -1,7 +1,7 @@
 module Environment where
 
 development :: Boolean
-development = true
+development = false
 
 backerCSSHash :: String
 backerCSSHash = "3e58580beabc10819188"
@@ -10,7 +10,7 @@ baseCSSHash :: String
 baseCSSHash = "be06c5112acf188d4cd8"
 
 commonJSHash :: String
-commonJSHash = "994b6b095200e02e3001"
+commonJSHash = "40c717753c6c4ee1ca51"
 
 emojiJSHash :: String
 emojiJSHash = "6d8cba9d21bf8311ebc2"
@@ -19,7 +19,7 @@ experimentsCSSHash :: String
 experimentsCSSHash = "2e9a38a44858c519247a"
 
 experimentsJSHash :: String
-experimentsJSHash = "80cd52a561d3284a4797"
+experimentsJSHash = "371b5356fb478ecd7c46"
 
 externalCSSHash :: String
 externalCSSHash = "9ec3c13777955bc00c64"
@@ -30,17 +30,17 @@ helpCSSHash = "0c86b8fed90c6933a556"
 helpJSHash :: String
 helpJSHash = "92426584a3de5109359f"
 
-imJSHash :: String
-imJSHash = "57399b3679a4a1dc3ed8"
-
 imCSSHash :: String
-imCSSHash = "eb7423b1f3bfa9d81915"
+imCSSHash = "4bdf39ce4a0552cdc822"
+
+imJSHash :: String
+imJSHash = "7cce0abb5e51d2dac829"
 
 internalHelpJSHash :: String
-internalHelpJSHash = "284b2a090c302acd9e69"
+internalHelpJSHash = "e7e21d3c3bc3d172da51"
 
 landingJSHash :: String
-landingJSHash = "92e9dca69eda3cc41676"
+landingJSHash = "b061f60cacc9cb5e3433"
 
 leaderboardCSSHash :: String
 leaderboardCSSHash = "77e6e27fc9961a9dba4d"
