60325e73d0ba0c03956f364e8a944361de74e473
Eduardo Asafe <e@asafe.dev>
2022-09-01 17:27:42 +0400

    Inline landing

--- inline-landing.sh
+++ inline-landing.sh
@@ -8,11 +8,10 @@ template=$(node -e 'import("./output-es/Server.Landing.Template/index.js").then(
 openStyle='<style type=text/css>'
 closeStyle='</style>'
 payload=$(echo "${template/$link/"$openStyle""$css""$closeStyle"}")
-handler="landing = function (v) {
-    return Control_Applicative.pure(Run.applicativeRun)(M.Html('$payload'));
+handler="function templateHack(v) {
+    return M.Html('$payload');
 };"
 
-sed -i -e 's/const landing/let landing/g' $file
-sed -i '1s/^/import \* as M from "..\/Shared.ContentType\/index.js";\n/' $file
+sed -i -e 's/const landing = v => Server$dResponse.serveTemplate(Server$dLanding$dTemplate.template);/const landing = v => Server$dResponse.serveTemplate(template);/' $file
 
 echo $handler >> $file
