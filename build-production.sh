3600a1fe18c648247524dc7c59d13f8b58b2e622
Eduardo Asafe <e@asafe.dev>
2022-09-04 00:11:12 +0400

    Sync resources

--- build-production.sh
+++ build-production.sh
@@ -7,11 +7,10 @@ bucket="ourmelon"
 git pull --no-edit &&
 npm install &&
 rm -rf dist/production &&
-rm -rf output &&
 rm -rf output-es &&
 spago -x production.dhall build &&
 npx webpack --progress --config webpack.production.config.js &&
-webpack --progress --config webpack.landing.config.js &&
+npx webpack --progress --config webpack.landing.config.js &&
 b2-linux sync --keepDays 7 --replaceNewer "$PWD/$dist_folder" "b2://$bucket/$bundle_folder" &&
 chmod +x ./inline-landing.sh &&
 ./inline-landing.sh &&
