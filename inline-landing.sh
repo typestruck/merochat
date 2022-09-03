5d3cfcd133ed7fa21cbd992a97481fc14f34cecc
Eduardo Asafe <e@asafe.dev>
2022-09-04 00:35:35 +0400

    Add lock

--- inline-landing.sh
+++ inline-landing.sh
@@ -2,7 +2,7 @@
 
 #hack to speed up landing page loading
 file="output-es/Server.Landing.Handler/index.js"
-link='\<link rel=\"stylesheet\" type=\"text\/css\" href=\"https\:\/\/static\.melan\.chat\/file\/ourmelon\/base\.*\.css\"\>'
+link='\<link rel=\"stylesheet\" type=\"text\/css\" href=\"https\:\/\/static\.melan\.chat\/file\/ourmelon\/bundle\/base\.*\.css\"\>'
 css=$(<dist/production/style.css)
 template=$(node -e 'import("./output-es/Server.Landing.Template/index.js").then(m => console.log(m.template()))')
 openStyle='<style type=text/css>'
