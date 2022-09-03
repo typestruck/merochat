d938898086e679400f8ff014f56e638320e3945d
Eduardo Asafe <e@asafe.dev>
2022-09-03 22:50:17 +0400

    Sync resources

--- client-production.sh
+++ client-production.sh
@@ -4,12 +4,4 @@ dist_folder="dist/production"
 bundle_folder="bundle"
 bucket="ourmelon"
 
-npm run build-production-client
-result=$?
-
-if [ "$result" -gt 0 ]; then
-  echo "BUILD FAILED"
-  exit 1
-fi
-
-b2-linux sync --keepDays 7 --replaceNewer "$PWD/$dist_folder" "b2://$bucket/$bundle_folder"
+npm run build-production-client && b2-linux sync --keepDays 7 --replaceNewer "$PWD/$dist_folder" "b2://$bucket/$bundle_folder"
