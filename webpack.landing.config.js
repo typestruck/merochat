85331ad36b9ebb4e7c84f161de047059ed9299e7
Eduardo Asafe <e@asafe.dev>
2022-09-04 21:28:09 +0400

    Inline landing stylesheet

--- webpack.landing.config.js
+++ webpack.landing.config.js
@@ -26,7 +26,7 @@ export default {
         new webpack.DefinePlugin({
             'process.env.PRODUCTION': true
         }),
-      //  new ReplaceHashPlugin({ files: [{ dir: 'dist/production', prefix: 'common' }, 'output-es/Shared.Resource/index.js'] }),
+        new ReplaceHashPlugin({ files: [{ dir: 'dist/production', prefix: 'common' }, 'output-es/Shared.Resource/index.js'] }),
         new InlineStylePlugin({ files: [{styleFile: 'dist/production/style.css', htmlFile: 'output-es/Server.Landing.Template/index.js'} ]})
     ],
     module: {
@@ -48,7 +48,7 @@ export default {
     optimization: {
         removeEmptyChunks: true,
         minimizer: [
-            // new TerserPlugin(),
+            new TerserPlugin(),
             new CssMinimizerPlugin(),
         ]
     },
