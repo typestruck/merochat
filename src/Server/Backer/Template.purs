ec2f0b4cb88df74aa3363850c911532b9475ef11
Eduardo Asafe <e@asafe.dev>
2022-09-17 20:04:02 +0400

    Add styles to external backer page

--- src/Server/Backer/Template.purs
+++ src/Server/Backer/Template.purs
@@ -8,12 +8,16 @@ import Flame.Html.Element as HE
 import Flame.Renderer.String as FRS
 import Server.Template (externalDefaultParameters)
 import Server.Template as ST
-import Shared.Resource as SPT
 import Shared.Backer.View as SBV
+import Shared.Resource (Bundle(..), ResourceType(..))
+import Shared.Resource as SP
 
 template ∷ Effect String
 template = do
       contents ← ST.template externalDefaultParameters
             { content = externalDefaultParameters.content <> [ SBV.view ]
+            , css = externalDefaultParameters.css <>
+                    [ HE.link [ HA.rel "stylesheet", HA.type' "text/css", HA.href $ SP.bundlePath Backer Css ]
+                    ]
             }
       FRS.render contents
