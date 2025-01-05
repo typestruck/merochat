8411225c1293037205cbb86a2a812f911c724e35
Eduardo Asafe <e@asafe.dev>
2025-01-05 16:04:19 -0300

    file

---
+++ src/Shared/Experiments/WordChain.purs
@@ -0,0 +1,13 @@
+module Shared.Experiments.WordChain where
+
+import Prelude
+
+import Flame (Html)
+import Flame.Html.Attribute as HA
+import Flame.Html.Element as HE
+import Shared.Experiments.Types (ChatExperimentMessage, ChatExperimentModel)
+
+view ∷ ChatExperimentModel → Html ChatExperimentMessage
+view model = HE.div (HA.class' "word-chain")
+      [ HE.button (HA.class' "green-button") "Play!"
+      ]
