f5879927320c9386d268eb9a0e08bc750ef6dcd2
Eduardo Asafe <e@asafe.dev>
2025-01-19 13:16:26 -0300

    Validate aac

--- src/Server/File.js
+++ src/Server/File.js
@@ -1,6 +1,6 @@
 import imageType from 'image-type';
 
-//webm for audio files
+//webm and aac for audio files
 function webm(buffer) {
     return buffer[0] === 26 &&
         buffer[1] === 69 &&
@@ -8,9 +8,14 @@ function webm(buffer) {
         buffer[3] === 163 ? 'webm' : '';
 }
 
+function aac(buffer) {
+    console.log(buffer[0], buffer[1]);
+    return buffer[0] === 255 && (buffer[1] === 241 || buffer[1] === 249) ? 'aac' : '';
+}
+
 export function realFileExtension_(buffer) {
-    return async function() {
+    return async function () {
         let r = await imageType(buffer);
-        return r.ext || webm(buffer) || '';
+        return r.ext || webm(buffer) || aac(buffer) || '';
     }
 }
