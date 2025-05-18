604ee28764ad8d685651718b5954ce1e74f47edd
Eduardo Asafe <e@asafe.dev>
2025-05-18 15:23:36 -0300

    Debug audios

--- src/Server/File.purs
+++ src/Server/File.purs
@@ -40,7 +40,7 @@ saveBase64File ∷ ∀ r. String → BaseEffect { configuration ∷ Configuratio
 saveBase64File input =
       case DS.split (Pattern ",") input of
             [ mediaType, base64 ] → do
-                  case DH.lookup (DSR.replace (DSRU.unsafeRegex "\\s*?codecs=.+;" noFlags) "" mediaType) allowedMediaTypes of
+                  case DH.lookup (spy "mt" (DSR.replace (DSRU.unsafeRegex "\\s*?codecs=.+;" noFlags) "" mediaType)) allowedMediaTypes of
                         Nothing → spy "iv" invalidImage
                         Just _ → do
                               buffer ← R.liftEffect $ NB.fromString base64 Base64
