3dd45cd4b5d737d2a211cd65d94e3c5ce0008c0c
Eduardo Asafe <e@asafe.dev>
2025-06-01 17:58:19 -0300

    pwa fixes

--- src/Client/Im/Pwa.js
+++ src/Client/Im/Pwa.js
@@ -23,8 +23,14 @@ export async function subscribe_(registration) {
     let vp = '[VAPID-PUBLIC-KEY-contenthash]',
         padding = '=',
         base64 = (vp + padding).replace(/\-/g, '+').replace(/_/g, '/'),
-        vapidPublicKey = Uint8Array.fromBase64(base64)
-        sub = registration.pushManager.subscribe({ userVisibleOnly: true, applicationServerKey: vapidPublicKey });
+        rawData = window.atob(base64),
+        vapidPublicKey = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+        vapidPublicKey[i] = rawData.charCodeAt(i);
+    }
+
+    let sub = registration.pushManager.subscribe({ userVisibleOnly: true, applicationServerKey: vapidPublicKey });
 
     return sub;
 }
