62bcaa1583564da81745cdd45f7054187257ec30
Eduardo Asafe <e@asafe.dev>
2025-06-07 10:39:31 -0300

    Update paths

--- src/Server/Push.js
+++ src/Server/Push.js
@@ -1,11 +1,17 @@
 export async function push_(url, topic, title, userId) {
+    console.log('j', JSON.stringify({
+        'topic': topic,
+        'message': '',
+        'title': title,
+        'click': userId
+    }));
     let r = await fetch(url, {
         method: 'POST',
         body: JSON.stringify({
             'topic': topic,
             'message': '',
             'title': title,
-            'click': userId
+            'click': userId + ''
         })
     });
 
