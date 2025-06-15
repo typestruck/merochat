a21132cce765ebfc79373ee2b125cc5d5c193616
Eduardo Asafe <e@asafe.dev>
2025-06-15 17:24:56 -0300

    Close if no connections

--- src/Server/WebSocket/Events.purs
+++ src/Server/WebSocket/Events.purs
@@ -158,7 +158,7 @@ handleClose token loggedUserId allUsersAvailabilityRef _ _ = do
       now ← EN.nowDateTime
       updatedAllUsersAvailability ← ER.modify (DH.update (removeConnection now) loggedUserId) allUsersAvailabilityRef
       let updatedUserAvaibility = SU.fromJust $ DH.lookup loggedUserId allUsersAvailability
-      when (DH.isEmpty updatedUserAvaibility.connections && userAvaibility.availability == Online) $ DF.traverse_ (sendTrackedAvailability updatedAllUsersAvailability updatedUserAvaibility.availability) updatedUserAvaibility.trackedBy
+      when (spy "spied closed" (DH.isEmpty updatedUserAvaibility.connections && userAvaibility.availability == Online)) $ DF.traverse_ (sendTrackedAvailability updatedAllUsersAvailability updatedUserAvaibility.availability) updatedUserAvaibility.trackedBy
 
       where
       removeConnection now userAvailability = Just $ makeUserAvailabity userAvailability (Left token) now None
