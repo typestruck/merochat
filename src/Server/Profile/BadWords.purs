e7ccc07367aa020e581483f6daf1a64063e3831c
Eduardo Asafe <e@asafe.dev>
2026-06-29 19:05:38 +0700

    m

--- src/Server/Profile/BadWords.purs
+++ src/Server/Profile/BadWords.purs
@@ -3,5 +3,5 @@ module Server.Profile.BadWords where
 import Prelude
 
 badWords ∷ Array String
-badWords = [ "pussy", "p*ssy", "dick", "d*ck", "cock", "women", "brick", "uncut", "ass", "gay", "relationship", "straight", "inches", "rp", "roleplay", "nude", "nudes", "n*des", "porn", "bitches", "sex", "sell", "bussy", "tits", "boobs", "cp", "incest", "taboo", "uncut", "hentai" ]
+badWords = [ "pussy", "p*ssy", "dick", "d*ck", "cock", "women", "brick", "uncut", "ass", "gay", "relationship", "straight", "inches", "rp", "roleplay", "nude", "nudes", "n*des", "porn", "bitches", "sex", "sell", "bussy", "tits", "boobs", "cp", "incest", "taboo", "uncut", "hentai", "meeting", "bottom" ]
 
